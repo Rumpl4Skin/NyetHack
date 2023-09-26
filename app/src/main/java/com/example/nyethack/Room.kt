56a62c9dd93499938099defb22d5df3fe657be0e
markk <mark.karchmit@yandex.ru>
2023-09-26 17:57:36 +0300

    oop practice part 7 add monster & MonsterRoom

--- app/src/main/java/com/example/nyethack/Room.kt
+++ app/src/main/java/com/example/nyethack/Room.kt
@@ -2,7 +2,7 @@ package com.example.nyethack
 
 open class Room(val name: String) {
     protected open val status = "Calm"
-    fun description() = "$name (Currently: $status)"
+    open fun description() = "$name (Currently: $status)"
     open fun enterRoom() {
         narrate("There is nothing to do here")
     }
@@ -18,5 +18,20 @@ class TownSquare : Room("The Town Square"){
 
     public fun ringBell() {
         narrate("The bell tower announces the hero's presence: $bellSound")
+    }
+}
+
+class MonsterRoom(
+    name: String,
+    var monster: Monster? = Goblin()
+):Room(name){
+    override fun description()=super.description()+ " (Creature: ${monster?.description ?: "None"})"
+
+    override fun enterRoom() {
+        if (monster == null) {
+            super.enterRoom()
+        } else {
+            narrate("Danger is lurking in this room")
+        }
     }
 }
